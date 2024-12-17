a3e10140347c9672be9ae1ecec782715465a4d69
sadanand chavan <sadanand.chavan@billdesk.com>
2024-12-17 12:41:19 +0530

    Spring boot consumer

---
+++ src/main/java/com/sada/sbkafkaConsumer/SbkafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.sada.sbkafkaConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbkafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbkafkaConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sada/sbkafkaConsumer/SbkafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sada.sbkafkaConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbkafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
